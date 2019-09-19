6780378aed6118b15c64ea0a5669896951141d52
albrdev <albrdev@gmail.com>
2019-09-19 14:07:04 +0200

    -

--- generic.cpp
+++ generic.cpp
@@ -38,7 +38,7 @@ const char* hexstr(const void* const data, const size_t size)
     const uint8_t* ptr = (const unsigned char*)data;
     size_t max = min(size, HEXSTRBUF_SIZE / 2);
     size_t j = 0U;
-    for(size_t i = 0U, j = 0U; i < max; i++)
+    for(size_t i = 0U; i < max; i++)
     {
         _hexstr_buf[j] = _hexchar_map[(ptr[i] & 0xF0) >> 4];
         j++;
