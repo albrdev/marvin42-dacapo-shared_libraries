79f72175701df932a93245a381be6f8ea02c592d
albrdev <albrdev@gmail.com>
2019-12-02 16:02:40 +0100

    functions for new types, changed joystick data packet

--- inc/custom_packets.h
+++ inc/custom_packets.h
@@ -6,14 +6,19 @@
 #include "packet.h"
 
 typedef struct _packet_motorrun packet_motorrun_t;
-typedef struct _packet_motorjsdata packet_motorjsdata_t;
+typedef struct _packet_motorrun2 packet_motorrun2_t;
+
+typedef struct _packet_orientationdata packet_orientationdata_t;
+typedef struct _packet_proximitydata packet_proximitydata_t;
 
 enum CustomPacketType
 {
     CPT_STATUS = PT_MAX,
-    CPT_MOTORRUN,
     CPT_MOTORSTOP,
-    CPT_MOTORJSDATA
+    CPT_MOTORRUN,
+    CPT_MOTORRUN2,
+    CPT_ORIENTATIONDATA,
+    CPT_PROXIMITYDATA
 };
 
 struct __attribute__((packed)) _packet_motorrun
@@ -24,12 +29,28 @@ struct __attribute__((packed)) _packet_motorrun
     single_t right;
 };
 
-struct __attribute__((packed)) _packet_motorjsdata
+struct __attribute__((packed)) _packet_motorrun2
 {
     packet_header_t header;
 
-    single_t balance;
-    int8_t direction;
+    single_t direction;
+    single_t power;
+};
+
+struct __attribute__((packed)) _packet_orientationdata
+{
+    packet_header_t header;
+
+    single_t velocity;
+    quaterniondata_t rotation;
+};
+
+struct __attribute__((packed)) _packet_proximitydata
+{
+    packet_header_t header;
+
+    single_t distance;
+    single_t angle;
 };
 
 #ifdef __cplusplus
@@ -37,8 +58,11 @@ extern "C"
 {
 #endif
 
-void packet_mkmotorrun(struct _packet_motorrun *const pkt, const float left, const float right);
-void packet_mkmotorjsdata(struct _packet_motorjsdata* const pkt, const float balance, const int8_t direction);
+void packet_mkmotorrun(struct _packet_motorrun *const pkt, const single_t left, const single_t right);
+void packet_mkmotorrun2(struct _packet_motorrun2* const pkt, const single_t direction, const single_t power);
+
+void packet_mkorientationdata(struct _packet_orientationdata* const pkt, const single_t velocity, const single_t w, const single_t x, const single_t y, const single_t z);
+void packet_mkproximitydata(struct _packet_proximitydata* const pkt, const single_t distance, const single_t angle);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- src/custom_packets.c
+++ src/custom_packets.c
@@ -1,7 +1,7 @@
 #include "custom_packets.h"
 #include "crc.h"
 
-void packet_mkmotorrun(struct _packet_motorrun *const pkt, const float left, const float right)
+void packet_mkmotorrun(struct _packet_motorrun *const pkt, const single_t left, const single_t right)
 {
     memcpy(&pkt->left, &left, sizeof(pkt->left));
     memcpy(&pkt->right, &right, sizeof(pkt->right));
@@ -9,10 +9,29 @@ void packet_mkmotorrun(struct _packet_motorrun *const pkt, const float left, con
     packet_mkheader(&pkt->header, sizeof(*pkt), CPT_MOTORRUN);
 }
 
-void packet_mkmotorjsdata(struct _packet_motorjsdata* const pkt, const float balance, const int8_t direction)
+void packet_mkmotorrun2(struct _packet_motorrun2* const pkt, const single_t direction, const single_t power)
 {
-    memcpy(&pkt->balance, &balance, sizeof(pkt->balance));
     memcpy(&pkt->direction, &direction, sizeof(pkt->direction));
+    memcpy(&pkt->power, &power, sizeof(pkt->power));
 
-    packet_mkheader(&pkt->header, sizeof(*pkt), CPT_MOTORJSDATA);
+    packet_mkheader(&pkt->header, sizeof(*pkt), CPT_MOTORRUN2);
+}
+
+void packet_mkorientationdata(struct _packet_orientationdata* const pkt, const single_t velocity, const single_t w, const single_t x, const single_t y, const single_t z)
+{
+    memcpy(&pkt->velocity, &velocity, sizeof(pkt->velocity));
+    memcpy(&pkt->rotation.w, &w, sizeof(pkt->rotation.w));
+    memcpy(&pkt->rotation.x, &x, sizeof(pkt->rotation.x));
+    memcpy(&pkt->rotation.y, &y, sizeof(pkt->rotation.y));
+    memcpy(&pkt->rotation.z, &z, sizeof(pkt->rotation.z));
+
+    packet_mkheader(&pkt->header, sizeof(*pkt), CPT_ORIENTATIONDATA);
+}
+
+void packet_mkproximitydata(struct _packet_proximitydata* const pkt, const single_t distance, const single_t angle)
+{
+    memcpy(&pkt->distance, &distance, sizeof(pkt->distance));
+    memcpy(&pkt->angle, &angle, sizeof(pkt->angle));
+
+    packet_mkheader(&pkt->header, sizeof(*pkt), CPT_PROXIMITYDATA);
 }
