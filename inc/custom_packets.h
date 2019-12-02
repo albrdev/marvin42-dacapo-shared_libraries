a8f1e3773a153e0d37e5d5faeb9205f84e7cf396
albrdev <albrdev@gmail.com>
2019-12-02 16:08:57 +0100

    update

--- inc/custom_packets.h
+++ inc/custom_packets.h
@@ -33,7 +33,7 @@ struct __attribute__((packed)) _packet_motorrun2
 {
     packet_header_t header;
 
-    single_t direction;
+    vector2data_t direction;
     single_t power;
 };
 
@@ -59,7 +59,7 @@ extern "C"
 #endif
 
 void packet_mkmotorrun(struct _packet_motorrun *const pkt, const single_t left, const single_t right);
-void packet_mkmotorrun2(struct _packet_motorrun2* const pkt, const single_t direction, const single_t power);
+void packet_mkmotorrun2(struct _packet_motorrun2* const pkt, const vector2data_t direction, const single_t power);
 
 void packet_mkorientationdata(struct _packet_orientationdata* const pkt, const single_t velocity, const single_t w, const single_t x, const single_t y, const single_t z);
 void packet_mkproximitydata(struct _packet_proximitydata* const pkt, const single_t distance, const single_t angle);
